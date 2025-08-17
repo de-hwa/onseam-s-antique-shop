308e7bf2e9bccc3da53496c83bcc71b960132fcc
de-hwa <dehwa8763@gmail.com>
2025-08-17 16:41:33 +0900

    20250817 login 백엔드 sql injection 대응

--- member/login_proc.php
+++ member/login_proc.php
@@ -2,10 +2,42 @@
 include "../include/db_connect.php";
 session_start();
 
-$f_id = $_POST['f_id'];
-$f_pw = $_POST['f_pw'];
+$f_id = urldecode($_POST['f_id']);
+$f_pw = urldecode($_POST['f_pw']);
 
-# ASM!!!!!!!!
+// Error Based SQL Injection 방지
+if(preg_match('/UNION\s+SELECT/i',$f_id)) {
+                  echo "<script>
+               alert(\"해당 문구는 사용하실 수 없습니다.\");
+               location.href=\"../member/login_form.php\";
+             </script>";
+                exit;
+}
+if(preg_match('/UNION\s+SELECT/i',$f_pw)) {
+                  echo "<script>
+               alert(\"해당 문구는 사용하실 수 없습니다.\");
+               location.href=\"../member/login_form.php\";
+             </script>";
+                exit;
+}
+// SQL Injection 방지
+if (!preg_match('/^[A-Za-z0-9._]+$/', $f_id)) {
+    echo "<script>
+        alert(\"아이디는 영문, 숫자, '.', '_' 만 사용할 수 있습니다.\");
+        location.href=\"../member/login_form.php\";
+    </script>";
+    exit;
+}
+
+if (strlen($f_pw) < 8 || strlen($f_pw) > 64) {
+    echo "<script>
+        alert(\"비밀번호는 8자 이상 64자 이하로 입력해야 합니다.\");
+        location.href=\"../member/login_form.php\";
+    </script>";
+    exit;
+}
+
+
 $sql = "SELECT * from users_info where id='$f_id'";
 $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_assoc($result);
